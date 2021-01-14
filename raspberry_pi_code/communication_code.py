307c048da142f25ef92c1403b8084ece14e442f4
Youssef Hussein] <youssefmohamed552@gmail.com>
2021-01-13 22:23:14 -0500

    forward and backwards

--- raspberry_pi_code/communication_code.py
+++ raspberry_pi_code/communication_code.py
@@ -26,33 +26,68 @@ gamepad_state = {
 }
 
 def adjust_value(num, state):
-  val = (num - 255) if state in ["ABS_Y", "ABS_X", "ABS_RZ", "ABS_Z"] else num
-  val = 1 if state in ["ABS_TR2", "ABS_TL2"] and num == 255 else 0
+  if state in ["ABS_RZ"]:
+    gamepad_state[state] = 128 - num
+  if state in ["ABS_Z"]:
+    gamepad_state[state] = num - 127
+  if state in ["ABS_TR2", "ABS_TL2"] and num == 255:
+    pass
+    # gamepad_state[state] = 1
+
+def get_command():
+  commands = [val for val in gamepad_state.keys() if gamepad_state[val] != 0]
+  print("gamepads: ", gamepad_state)
+  print("commands: ", commands)
+  if len(commands) == 0:
+    return "S"
+  if len(commands) == 1:
+    c = commands[0]
+    if c == "ABS_RZ":
+      if gamepad_state[c] > 0:
+        return "F " + str(gamepad_state[c])
+      elif gamepad_state[c] < 0:
+          return "B " + str(-1 * gamepad_state[c])
+    if c == "ABS_Z":
+      if gamepad_state[c] > 0:
+        return "R " + str(gamepad_state[c])
+      elif gamepad_state[c] < 0:
+        return "L " + str(-1 * gamepad_state[c])
+  if len(commands) == 2:
+    if all(element in ["ABS_RZ", "ABS_Z"] for element in commands):
+      return "N "
+    if all(element in ["ABS_RZ", "BTN_TR2"] for element in commands):
+      print("raspberryPi moving forward")
+  return "M"
+
 
 
 
 def handleJoystick():
   print("in handling joystick")
-with serial.Serial('/dev/ttyACM0', 115200, timeout=0.1) as arduino:
+  with serial.Serial('/dev/ttyACM0', 115200, timeout=0.1) as arduino:
     while True:
       events = inputs.get_gamepad()
       for event in events:
         print("ev_type: ", event.ev_type, " code: ", event.code, " type: ", type(event.code), " state: ", event.state)
         if event.ev_type == "Key" or event.ev_type == "Absolute":
-          handleSerialWrite(arduino, event.code, event.state)
+          adjust_value(event.state, event.code)
+          if event.code in ["ABS_RZ", "ABS_Z"]:
+            handleSerialWrite(arduino)
           # test_serial(arduino, event.code, event.state)
 
 
 
 
-def handleSerialWrite(dev, code, value):
+def handleSerialWrite(dev):
   print("in test serial")
-  dev.write(str.encode(gamepad_map[code] + str(value)))
+  cmd = get_command()
+  print("cmd: ", cmd)
+  dev.write(str.encode(cmd))
   # time.sleep(0.1)
-  while True:
-    data = dev.readline()
-    if data:
-      print(data[:-2])
+  # while True:
+    # data = dev.readline()
+    # if data:
+      # print(data[:-2])
       # break
 
 
